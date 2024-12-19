8bbe9e270663fdf85bc9543698d59d1b1dca8313
PeterNgeno <peterkipsangngeno@gmail.com>
2024-12-19 21:57:16 +0300

    Add files via upload

---
+++ services/paymentService.js
@@ -0,0 +1,8 @@
+// services/paymentService.js
+
+module.exports = {
+  processPayment: (amount, paymentDetails) => {
+    // Logic to process payment (this is a placeholder)
+    console.log(`Processing payment of ${amount} for ${paymentDetails}`);
+  },
+};
