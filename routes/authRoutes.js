4e7887c36aac489ed7f1188afcfddd976b1083d8
PeterNgeno <peterkipsangngeno@gmail.com>
2025-01-31 20:11:23 +0300

    Update authRoutes.js
    Signed-off-by: PeterNgeno <peterkipsangngeno@gmail.com>

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs'); // Replaced bcrypt with bcryptjs
 const { db } = require('../firebase'); // Firestore instance
 
 // POST route for signing up
